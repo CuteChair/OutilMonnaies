8ef2f0bba965932d12335761c9f827ec1f668f79
Chair <jules.lhp@gmail.com>
2023-09-28 11:19:05 -0400

    Clarification et message d'erreur suppl

--- outilMonnaies/Program.cs
+++ outilMonnaies/Program.cs
@@ -8,12 +8,13 @@ Console.WriteLine("Bonjour, quelle est votre monnaie de départ? ");
 Console.Write("Choisir entre CAD$ (1) ou USD$ (2) ou EUR (3) :");
 choix = Convert.ToDouble(Console.ReadLine());
 
+//Selection CAD
 
 if (choix == 1)
 {
 
 
-
+    qust1:
     Console.WriteLine("Vous avez choisi CAD! ");
     Console.Write("Voulez vous convertir en USD (1) ou EUR (2)? ");
     choix2 = Convert.ToDouble(Console.ReadLine());
@@ -33,10 +34,23 @@ if (choix == 1)
         eur = cad / 1.4251391;
         Console.WriteLine($"Le montant {cad} CAD equivaut a {eur} EUR !");
     }
+    if (choix2 > 2)
+    {
+        Console.WriteLine("Veuillez selectionner un chiffre entre 1 et 2");
+        goto qust1;
+    }
+    if (choix2 < 1)
+    {
+        Console.WriteLine("Veuillez selectionner un chiffre entre 1 et 2");
+        goto qust1;
+    }
 }
+
+//Selection USD
 
 if (choix == 2)
 {
+    qust1:
     Console.WriteLine("Vous avez choisi USD! ");
     Console.Write("Voulez vous convertir en CAD (1) ou EUR (2)? ");
     choix2 = Convert.ToDouble(Console.ReadLine());
@@ -56,11 +70,23 @@ if (choix == 2)
         eur = us * 0.94672824;
         Console.WriteLine($"Le montant {us} USD equivaut a {eur} EUR !");
     }
-
+    if (choix2 > 2)
+    {
+        Console.WriteLine("Veuillez selectionner un chiffre entre 1 et 2");
+        goto qust1;
+    }
+    if (choix2 < 1)
+    {
+        Console.WriteLine("Veuillez selectionner un chiffre entre 1 et 2");
+        goto qust1;
+    }
 }
+
+//selection EUR
 
 if (choix == 3)
 {
+    qust1:
     Console.WriteLine("Vous avez choisi EUR! ");
     Console.Write("Voulez vous convertir en CAD (1) ou USD (2)? ");
     choix2 = Convert.ToDouble(Console.ReadLine());
@@ -80,7 +106,16 @@ if (choix == 3)
         us = eur / 0.94672824;
         Console.WriteLine($"Le montant {eur} EUR equivaut a {us} USD$ !");
     }
-
+    if (choix2 > 2)
+    {
+        Console.WriteLine("Veuillez selectionner un chiffre entre 1 et 2");
+        goto qust1;
+    }
+    if (choix2 < 1)
+    {
+        Console.WriteLine("Veuillez selectionner un chiffre entre 1 et 2");
+        goto qust1;
+    }
 }
 
 if (choix > 3)
