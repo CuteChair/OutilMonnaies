c45a16e3c2f9ce0260dd29bd103c17ddd746d6e8
Chair <jules.lhp@gmail.com>
2023-09-28 10:43:27 -0400

    EUR to CAD US

--- outilMonnaies/Program.cs
+++ outilMonnaies/Program.cs
@@ -2,21 +2,23 @@
 
 using System.Linq.Expressions;
 
-double cad = 0, us = 0, choix = 0;
+double cad = 0, us = 0, eur = 0, choix = 0, choix2 = 0;
 question:
 Console.WriteLine("Bonjour, quelle est votre monnaie de départ? ");
-Console.Write("Choisir entre CAD$ (1) ou USD$ (2) :");
+Console.Write("Choisir entre CAD$ (1) ou USD$ (2) ou EUR :");
 choix = Convert.ToDouble(Console.ReadLine());
 
    
-    
+    /*
     if (choix == 1)
     {
         Console.WriteLine("Vous avez choisi CAD!");
-        Console.Write("Pour convertir en USD entrer le montant :");
-        cad = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Voulez vous convertir en USD (1) ou en EUR (2)?");
+        choix2 = Convert.ToDouble(Console.ReadLine());
+    cad = Convert.ToDouble(Console.ReadLine());
         us = cad * 0.74;
         Console.WriteLine($"Le montant {cad} CAD$ equivaut a {us} USD$ !");
+
     } 
    
     if (choix == 2)
@@ -27,6 +29,30 @@ choix = Convert.ToDouble(Console.ReadLine());
         cad = us / 0.74;
         Console.WriteLine($"Le montant {us} USD$ equivaut a {cad} CAD$ !");
     }
+    */
+    if(choix == 3)
+{
+    Console.WriteLine("Vous avez choisi EUR! ");
+    Console.Write("Voulez vous convertir en CAD (1) ou USD (2)?");
+    choix2 = Convert.ToDouble(Console.ReadLine());
+    if (choix2 == 1)
+    {
+        Console.WriteLine("Vous avez choisi CAD!");
+        Console.Write("Entrez un montant :");
+        eur = Convert.ToDouble(Console.ReadLine());
+        cad = eur / 1.42;
+        Console.WriteLine($"Le montant {eur} EUR equivaut a {cad} CAD$ !");
+    }
+    if (choix2 == 2)
+    {
+        Console.WriteLine("Vous avez choisi USD!");
+        Console.Write("Entrez un montant :");
+        eur = Convert.ToDouble(Console.ReadLine());
+        us = eur / 0.95;
+        Console.WriteLine($"Le montant {eur} EUR equivaut a {us} USD$ !");
+    }
+   
+}
 
 Console.ReadKey();
 
